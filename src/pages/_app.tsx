21d3ab23115c4c374e09f45e47d5c5cf730608f0
100x developer <developer@sharduls-MacBook-Pro.local>
2026-04-07 23:34:44 +0530

    build-error-fixed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,12 +14,13 @@ export default function App({ Component, pageProps }: AppProps) {
       <AuthProvider>
         <GoogleAnalytics />
         <GoogleTranslateScript />
-        {/* Standard AdSense load to avoid data-nscript warnings */}
-        <script
+        <Script
+          id="adsense-script"
           async
+          strategy="afterInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5434867604639566"
           crossOrigin="anonymous"
-        ></script>
+        />
         <Toaster />
         <Sonner />
         <Component {...pageProps} />
